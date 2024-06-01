5823996a2e53956312fa5832a0d7b79f2ea56ba7
SpiridovaMaria <lexus754@mail.ru>
2024-06-01 11:03:20 +0300

    Final project

--- src/App.js
+++ src/App.js
@@ -10,69 +10,105 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 function App() {
   return (
     <section>
+      <ShowNavBar/>
+        <container class="mx-5">
+          <FilterAndSearchBooks/>
+        </container>
+    </section>
+    
+  );
+}
+
+
+const ShowNavBar =()=>{
+    return (
       <nav class="navbar navbar-light bg-light mb-3" style={{background:'#FFDEAD'}}>
         <div class="container-fluid bg-danger">
           <div class="d-flex align-items-center">
             <img class="mx-2" style={{width:'30px'}}src="https://cdn-icons-png.flaticon.com/512/2702/2702134.png"/>
             <a class="navbar-brand">My bookshop</a>
           </div>
-            <Dropdown class="my-2">
-              <Dropdown.Toggle variant="dark" id="dropdown-basic">
-                Genres
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href="">Art</Dropdown.Item>
-                <Dropdown.Item href="">Biography</Dropdown.Item>
-                <Dropdown.Item href="">Children</Dropdown.Item>
-                <Dropdown.Item href="">Fantasy</Dropdown.Item>
-                <Dropdown.Item href="">History</Dropdown.Item>
-                <Dropdown.Item href="">Medicine</Dropdown.Item>
-                <Dropdown.Item href="">Music</Dropdown.Item>
-                <Dropdown.Item href="">Mystery and Detectives</Dropdown.Item>
-                <Dropdown.Item href="">Plays</Dropdown.Item>
-                <Dropdown.Item href="">Recipes</Dropdown.Item>
-                <Dropdown.Item href="">Religion</Dropdown.Item>
-                <Dropdown.Item href="">Romance</Dropdown.Item>
-                <Dropdown.Item href="">Science Fiction</Dropdown.Item>
-                <Dropdown.Item href="">Textbooks</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-  </div>
-</nav>
-      
-    <container class="mx-5">
-      <ListBooks/>
-    </container>
-    </section>
-    
-  );
+         </div>
+     </nav> 
+    );
 }
-function ListBooks(){
+
+
+function FilterAndSearchBooks(){
   const BASE_URL="https://openlibrary.org/search.json"
   const[books,setBooks]=useState([]);
   useEffect(()=> {
     const fetchData= async()=>{
-      const response = await fetch(BASE_URL+"?q=subject=romance");
+      const response = await fetch(BASE_URL+"?q=harry%20potter&limit=30");
       const jsonData = await response.json();
-      console.log(jsonData);
       setBooks(jsonData.docs);
-      console.log(jsonData)
     }
     fetchData();
   },[]);
-  console.log(books)
+  console.log(books);
+    
+ //фильтрация    
+  const publishers = Array.from(new Set(books.map(function(item){
+      if (item.publisher!=null){
+        var sortedKeys = Object.keys(item.publisher).sort()
+        return item.publisher[sortedKeys[0]];   
+      }
+  })));
+  const[filterTerm,setFilterTerm] = useState('');
+  const filterChange=(event)=> {
+    setFilterTerm(event.target.value);
+  }
+  
+  //поиск
+  const[searchTerm, setSearchTerm] = useState('');
+  const[searchPermTerm, setSearchPermTerm] = useState('');
+  const handleSearchChange = (event) => {
+    setSearchPermTerm(event.target.value);
+  }
+  const searchChange = (event) => {
+    setSearchTerm(searchPermTerm);
+  }
+    
+  //карточки, подходящие по поиску и фильтрации
+  const searchCards = books.filter(function(item){
+    return filterTerm!="" ? String(item.title).toLowerCase().includes(searchTerm.toLowerCase()) & String(item.publisher).includes(filterTerm): String(item.title).toLowerCase().includes(searchTerm.toLowerCase());
+  });
+    
   return (
-    <div class="row row-cols-1 row-cols-lg-2 h-75 d-flex justify-content-between mx-5">
-      { books.map(function(item){
+      <div>
+        <div class="d-flex justify-content-center mb-5">
+            <div class="input-group w-25">
+                <input type="search" placeholder="Search for title" class="form-control shadow-none" onChange={handleSearchChange} />
+                <button onClick={searchChange} type="button" class="btn btn-danger">
+                    <i class="bi bi-search"></i>
+                </button>
+            </div>
+            <div class="form-outline mx-3">
+              <select class="form-select col-6 shadow-none" aria-label="Choose category" onChange={filterChange}>
+                  <option value=''>Choose the publisher</option>
+                  <ListCategories list={publishers}/>
+               </select>
+            </div>
+        </div>
+        <ShowBookCard list={searchCards}/>
+    </div>  
+  );
+}
+
+
+const ShowBookCard = (props) =>{
+  return(
+     <div class="row row-cols-1 row-cols-lg-2 h-75 d-flex justify-content-between mx-5">
+      { props.list.map(function(item){
         return <div class="card p-0 mb-3 mx-1" style={{maxWidth: "530px"}}>
         <div class="row g-0 w-100">
-          <img src={"https://covers.openlibrary.org/b/id/"+item.cover_i+'.jpg'} class="col-4 h-100 ps-0 img-fluid rounded-start" alt="Picture is not available"/>
+          <img src={"https://covers.openlibrary.org/b/id/"+item.cover_i+'.jpg'} class="object-fit-fill h-md-125 col-md-4 ps-0 img-fluid rounded-start" alt="Picture is not available"/>
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">'{item.title}'</h5>
               <p class="card-text">Author: {item.author_name}</p>
               <p class="card-text">Publication Year: {item.first_publish_year}</p>
-              <a class="btn btn-primary" href={'https://openlibrary.org/'+item.key}>Read more</a>
+              <a class="btn btn-danger" href={'https://openlibrary.org/'+item.key}>Read more</a>
             </div>
           </div>
         </div>
@@ -82,4 +118,15 @@ function ListBooks(){
   );
 }
 
+
+const ListCategories = (props) => {
+  return( <>
+    {props.list.map(function(item){
+      return <option>{item}</option>
+    })}
+  </>);
+}
+
+
+// список категорий
 export default App;
